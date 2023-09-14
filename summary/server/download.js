fc4d64d54ac8c65c5b1f173b9bc9d9643b2b61e9
Kleiton Silva Souza de Azevedo <kleiton.ads@hotmail.com>
2023-09-14 18:59:18 -0300

    add final files
    project done

--- summary/server/download.js
+++ summary/server/download.js
@@ -1,21 +1,29 @@
-import ytdl from 'ytdl-core'
-import fs from 'fs'
+import ytdl from "ytdl-core"
+import fs from "fs"
 
-export const download = (videoId) =>{
+export const download = (videoId) =>
+  new Promise((resolve, reject) => {
+   
+    const videoURL = "https://youtube.com/shorts/" + videoId
+    console.log("Realizando o download do vídeo:", videoId)
 
-  const videoURL = "https://youtube.com/shorts/" + videoId
-  console.log("Realizando o download do vídeo:" , videoId)
-
-  ytdl(videoURL, { quality:"lowestaudio", filter: "audioonly"}).on("info", (info) =>{
-  const seconds = info.formats[0].approxDurationMs / 1000
-    if(seconds > 60){
-      throw new Error ("A duração desse vídeo é maior do que 60 segundos.")
-    }
-    }
-  ).on("end", () => {
-    console.log("Download do vídeo finalizado.")
-  }).on("error", (error) => {
-    console.log("Não foi possível fazer o download do vídeo. Detalhes do erro:", error )
-  }).pipe(fs.createWriteStream("./tmp/audio.mp4"))
-
-}
+    ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" })
+      .on("info", (info) => {
+        const seconds = info.formats[0].approxDurationMs / 1000
+        if (seconds > 60) {
+          throw new Error("A duração desse vídeo é maior do que 60 segundos.")
+        }
+      })
+      .on("end", () => {
+        console.log("Download do vídeo finalizado.")
+        resolve()
+      })
+      .on("error", (error) => {
+        console.log(
+          "Não foi possível fazer o download do vídeo. Detalhes do erro:",
+          error
+        )
+        reject(error)
+      })
+      .pipe(fs.createWriteStream("./tmp/audio.mp4"))
+  })
--- summary/web/main.js
+++ summary/web/main.js
@@ -1,3 +1,7 @@
+// Js
+import "./form.js"
+
+// Css
 import "./styles/base.css"
 import "./styles/app.css"
 import "./styles/form.css"
